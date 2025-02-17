db78635dd67daba7b53245cf141664cae7c6c040
DFLH <danielleon2520@gmail.com>
2025-02-17 18:45:31 -0500

    Archivos de Configuracion

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        system.out,println("hola Mundo");
+
+        system.out,println("comandos git");
+
+        }
+    }
